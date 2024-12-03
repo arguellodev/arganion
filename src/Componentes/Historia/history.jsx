f9d8f03a55169fc8dc69c9c9ffee4ec0cd3d9dc5
arguellodev <arguellomty@gmail.com>
2024-12-02 22:02:47 -0600

    Cambio en el componente history

--- src/Componentes/Historia/history.jsx
+++ src/Componentes/Historia/history.jsx
@@ -1,42 +1,27 @@
-import './history.css';
+import "./history.css";
 
-const History = ()=>{
-    return(<>
-      <h1 className='titulo-history'>Nuestra historia</h1>
-        <section className="history-section">
-          
-          <div className='history-card'>
-            <div className='history-card-img-container'>
-            <img src='logo.svg'></img>
-            </div>
-            <p className='borde'></p>
-            <p className='texto-history'>Nuestro Logo representa la esencia surrealista e icónica de Argánion. La cabra, trepando por una rama del árbol de argán, simboliza curiosidad, audacia y alcanzar lo inusual, cualidades que definen a nuestro negocio</p>
-
+const History = () => {
+  return (
+    <>
+      <h1 className="titulo-history">Nuestra historia</h1>
+      <section className="history-section">
+        <div className="history-card">
+          <div className="history-card-img-container">
+            <img src="goats-in-trees.svg"></img>
           </div>
+          <p className="borde"></p>
+          <p className="texto-history">
+            Nuestro logo representa a una cabra escalando la rama de un arbol de{" "}
+            <span classname="negritas">Argán</span>, ya que su fruto es de deseo
+            para ellas . La cabra, trepando por una rama del árbol de argán,
+            simboliza curiosidad, audacia y alcanzar lo inusual, cualidades que
+            definen a nuestro negocio. La idea fue inspirada gracias a la
+            canción <span>Goats in Trees</span> de Foster The People
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
 
-          <div className='history-card'>
-          <div className='history-card-img-container'>
-            <img src='goats-in-trees.svg'></img>
-            </div>
-            <p className='borde'></p>
-            <p className='texto-history'>Estas cabras no escalan por simple instinto, sino por una motivación clara: alcanzar los frutos del argán, que tanto desean. Su tenacidad para superar cualquier obstáculo es una metáfora de nuestra filosofía de trabajo. Nos motiva su perseverancia, y la hacemos nuestra en cada proyecto que emprendemos</p>
-
-          </div>
-
-          <div className='history-card'>
-          <div className='history-card-img-container'>
-            <img src='logo-texto-negro.svg'></img>
-            </div>
-            <p className='borde'></p>
-            <p className='texto-history'>Aquí entra el toque tecnológico. Resaltamos el "ION" para mostrar la conexión con la tecnología digital: el flujo binario de 0s y 1s, que simbolizan input/output, creatividad y eficiencia, el núcleo de lo que hacemos</p>
-
-          </div>
-
-
-
-        </section>
-        </>
-    )
-}
-
-export default History
+export default History;
